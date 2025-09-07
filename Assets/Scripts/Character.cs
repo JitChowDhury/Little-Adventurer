a805e36456cff31522a504217cf870fcced4b73c
JitChowDhury <jitchowdhury8717@gmail.com>
2025-09-07 19:15:31 +0530

    Player Movement

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private CharacterController characterController;
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector3 movementVelocity;
+    private PlayerInput playerInput;
+    private float verticalVelocity;
+    private float gravity = -9.8f;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void CalculatePlayerMovement()
+    {
+        movementVelocity.Set(playerInput.horizontalInput, 0f, playerInput.verticalInput);
+        movementVelocity.Normalize();
+
+        movementVelocity = Quaternion.Euler(9, -45f, 0) * movementVelocity;
+        movementVelocity *= moveSpeed * Time.deltaTime;
+        if (movementVelocity != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(movementVelocity);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CalculatePlayerMovement();
+
+        if (!characterController.isGrounded)
+        {
+            verticalVelocity = gravity;
+        }
+        else
+        {
+            verticalVelocity = gravity * 0.3f;
+        }
+
+        movementVelocity += verticalVelocity * Vector3.up * Time.deltaTime;
+
+        characterController.Move(movementVelocity);
+
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnDisable()
+    {
+        horizontalInput = 0;
+        verticalInput = 0;
+    }
+}
